6ff2ecc4809e75bba8ba92b43af9f51884f16e12
quokt <158350772+quokt@users.noreply.github.com>
2024-01-31 23:35:13 +0100

    Add files via upload

--- game.bash
+++ game.bash
@@ -23,26 +23,45 @@ get_content() {
 
 #get target index from input
 get_target() {
-	get_content | jshon -C -Q -e inputs -e $1 -u |  cat
+	get_content | jshon -C -Q -e inputs -e "$1" -u |  cat
 }
 
+#read and display the json field passed as argument
+read_and_display() {
+	get_content | jshon -C -Q -e "$1"
+	say `get_content | jshon -C -Q -e "$1"`
+}
 
 ######### Main #########
 
 while $game_on ; do
 
 	#echo $current_index
-	get_content | jshon -C -Q -e display_name
-	say `get_content | jshon -C -Q -e display_name`
-	get_content | jshon -C -Q -e text
-	say `get_content | jshon -C -Q -e text`
+	#get_content | jshon -C -Q -e display_name
+	#say `get_content | jshon -C -Q -e display_name`
+	#get_content | jshon -C -Q -e text
+	#say `get_content | jshon -C -Q -e text`
+
+	read_and_display display_name
+	read_and_display text
 
 	read -p ">>>>>>>" input _trash
 	#get_target $input
-	target=`get_target $input`
+	target=`get_target "$input"`
+
 	#echo $target
 
-	current_index=$target
+	if [ "$input" = "exit" ]; then
+                game_on=false
+        fi
+
+	if [ "$target" = "null" ]; then
+		echo "target is null!"
+		#game_on=false
+	else
+		current_index=$target
+	fi
+
 
 
 	#echo $current_index
