d6d6d878f033f67d42db1fb92204458043d3cba2
Quokt <bug87@hotmail.fr>
2026-07-13 20:53:46 +0200

    replace jshon with jq. add config file

--- game.bash
+++ game.bash
@@ -4,13 +4,9 @@
 
 ######## Global Variables ########
 
+source ./game.conf
+
 game_on=true
-
-max_char_per_text_line=66
-language="fr"
-
-content_path="$HOME/jonba/"
-content_file_name="content.json"
 
 current_index="start"
 
@@ -20,35 +16,40 @@ null_count=0
 ######### Functions #########
 
 say() {
-	spd-say "$*" -r -100 -l $language -w
+	spd-say "$*" -r ${read_speed} -l ${language} -w
 }
 
 #get current content description from JSON
 get_content() {
-	jshon -C -Q -F $content_path$content_file_name -e $current_index
+	jq .${current_index} ${content_path}${content_file_name}
 }
 
 #get target index from input
 get_target() {
-	get_content | jshon -C -Q -e inputs -e "$1" -u |  cat
+	get_content | jq .inputs."${1}" | cat
 }
 
 get_and_display_actions() {
-	actions=$(get_content | jshon -C -Q -e inputs | jshon -C -Q -k)
-   	echo "$actions" | while read -r action; do
-        echo "- $action"
+	actions=$(get_content | jq '.inputs | keys[]')
+   	echo "${actions}" | while read -r action; do
+        echo "- ${action}"
     done
 }
 
 #read and display the json field passed as argument
 read_and_display() {
-	get_content | jshon -C -Q -e "$1" | fold -s -w $max_char_per_text_line
-	say `get_content | jshon -C -Q -e "$1"`
+	get_content | jq ."${1}" | fold -s -w ${max_char_per_text_line}
+	say `get_content | jq ."${1}"`
 }
 
 ######### Main #########
 
-while $game_on ; do
+clear
+
+jq --version >/dev/null 2>&1 || (echo "Please install jq" && exit 1)
+spd-say " " >/dev/null 2>&1 || (echo "Please install speech-dispatcher" && exit 1)
+
+while ${game_on} ; do
 
 	read_and_display display_name
 	read_and_display text
@@ -57,24 +58,25 @@ while $game_on ; do
 	get_and_display_actions
  	echo "########################"
 	read -r -p ">>>>>>> " input
-	target=$(get_target "$input")
 
-	case $input in
-	"exit" | "quit" | :?q? | :q? | :?q ) game_on=false;;
-	"esc" ) cat $content_path/game.bash | spd-say -e -w;;
+	case ${input} in
+	"exit" | "quit" | ":?q?" | ":q" | ":?q" ) game_on=false;;
+	"esc" ) cat ${content_path}/game.bash | spd-say -e -w;;
 	#*) echo "test"
 	esac
 
-#	if [ "$input" = "exit" ]; then
- #               game_on=false
-  #      fi
+	if [ ${game_on} = "false" ]; then
+		exit 0
+	fi
 
-	if [ "$target" = "null" ]; then
+        target=$(get_target "${input}")
+
+	if [ "${target}" = "null" ]; then
 		echo "target is null!"
 		#game_on=false
-		#null_count= $(($null_count + "1"))
-		#echo $null_count
+		#null_count= $((${null_count} + "1"))
+		#echo ${null_count}
 	else
-		current_index=$target
+		current_index=${target}
 	fi
 done
